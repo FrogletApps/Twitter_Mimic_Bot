7aeea63cf9f8921eff3e71de45007a8052809482
James Pearson <james_pearson@outlook.com>
2020-04-18 18:18:10 +0100

    Update mimic.py

--- mimic.py
+++ mimic.py
@@ -4,6 +4,10 @@
 from twython import Twython
 from secret import *
 
+#This will get test information from file
+def getTweetsTest():
+    
+
 def outputToTwitter(user, tweet):  
     twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     twitter.update_status(status= user + "\n" + tweet)
