7e3dfab3e3df2fde3d5ad979569fe079f4672a9b
James Pearson <james_pearson@outlook.com>
2020-04-27 19:31:45 +0100

    array to list

--- mimic.py
+++ mimic.py
@@ -11,34 +11,34 @@ from twython import Twython
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 #Insert a Twitter username here
-userToMimic = "realdonaldtrump"
+userToMimic = "vexroboticsuk"
 
 def getTweetsTest(fileName):
     '''
-    This will get test information from file and put it into an array (in lower case)
+    This will get test information from file and put it into a list (in lower case)
     Parameters:
         filename (string):  Location of the test data file
     Returns:
-        wordArray (string[]):  Array of words in file in lower case.
+        wordList (string[]):  List of words in file in lower case.
     '''
     with open(fileName, "r") as inputFile:
-        return inputFile.read().lower().split() #Set to lower case and split into array of words
+        return inputFile.read().lower().split() #Set to lower case and split into list of words
 
 def readTweetsByUser(username, limit=200, retweets=False):
     '''
-    This will get tweets from Twitter and put them into an array of tweets
+    This will get tweets from Twitter and put them into an list of tweets
     Parameters:
         username (string):  The user whose tweets you want to recieve
         limit (int):  The number of tweets you want to recieve (this is the maximum you can get, there is a limit of 200, and even if you disable retweets they are still counted) (default:200)
         retweets (Boolean):  Enable retweets or not (default:False)
     Returns:
-        tweetArray (string[]):  Array of tweets
+        tweetList (string[]):  List of tweets
     '''
     username = "@" + username
-    #dataArray contains a lot of metadata that we don't need
-    dataArray = twitter.get_user_timeline(screen_name=username, count=limit, include_rts=retweets, tweet_mode='extended')
-    tweetArray = []
-    for tweetData in dataArray:
+    #dataList contains a lot of metadata that we don't need
+    dataList = twitter.get_user_timeline(screen_name=username, count=limit, include_rts=retweets, tweet_mode='extended')
+    tweetList = []
+    for tweetData in dataList:
         #print(tweetData)
         #print()
         mediaType = ""
@@ -53,32 +53,32 @@ def readTweetsByUser(username, limit=200, retweets=False):
         #Collect Tweet, media type, and the media URL
         tweet = [tweetText, mediaType, mediaURL]
         #print(tweet)
-        tweetArray.append(tweet)
-    return tweetArray
+        tweetList.append(tweet)
+    return tweetList
 
-def getInputTweetsStats(tweetArray):
+def getInputTweetsStats(tweetList):
     '''
     This will calculate statistics about the tweets
     Parameters:
-        tweetArray (string[]):  Array of tweets
+        tweetList (string[]):  List of tweets
     Returns:
         outputStats (dict):  Dictionary of statistics
     '''
-    wordArray = []
+    wordList = []
     stats = []
     outputStats = {}
-    tweetNo = len(tweetArray)
+    tweetNo = len(tweetList)
 
-    for miniTweetData in tweetArray:
+    for miniTweetData in tweetList:
         tweetStats = []
         tweet = miniTweetData[0]
         #print(tweet)
-        wordArray = tweet.split()
-        tweetLength = len(wordArray)
+        wordList = tweet.split()
+        tweetLength = len(wordList)
         tweetStats.append(tweetLength)
 
         punctCount = 0
-        for word in wordArray:
+        for word in wordList:
             if word[-1:] in ["!", "?" ,"."]:
                 punctCount += 1
             
@@ -113,18 +113,18 @@ def getInputTweetsStats(tweetArray):
 
     return outputStats
 
-def splitIntoWords(tweetArray):
+def splitIntoWords(tweetList):
     '''
     This will get the tweets and split them up into individual words (and remove certain unwanted elements like @usernames and hyperlinks)
     Parameters:
-        tweetArray (string[]):  Array of tweets
+        tweetList (string[]):  List of tweets
     Returns:
-        wordArray (string[]):  Array of words from the user's tweets
-        firstWordArray (string[]):  Array of the first word used in a user's tweets
+        wordList (string[]):  List of words from the user's tweets
+        firstWordList (string[]):  List of the first word used in a user's tweets
     '''
-    wordArray = []
-    firstWordArray = []
-    for tweet in tweetArray:
+    wordList = []
+    firstWordList = []
+    for tweet in tweetList:
         firstWord = True
         #print(tweet)
         #print()
@@ -134,20 +134,20 @@ def splitIntoWords(tweetArray):
             if "@" not in word and "http" not in word:
                 #Strip out double quotes and brackets
                 word = re.sub("\"|“|”|\(|\)", "", word)
-                wordArray.append(word)
+                wordList.append(word)
                 if firstWord == True:
-                    firstWordArray.append(word)
+                    firstWordList.append(word)
                     firstWord = False
 
-    #print(wordArray)
-    print(firstWordArray)
-    return [wordArray, firstWordArray]
+    #print(wordList)
+    print(firstWordList)
+    return [wordList, firstWordList]
 
-def createDictionary(wordArray):
+def createDictionary(wordList):
     '''
     This will put the data into two dictionaries
     Parameters:
-        wordArray (string[]):  Array of words
+        wordList (string[]):  List of words
     Returns:
         integerToString (dict):  Dictionary arranged by integers and storing strings
         stringToInteger (dict):  Dictionary arranged by strings and storing integers
@@ -156,7 +156,7 @@ def createDictionary(wordArray):
     counterSTI = 0
     integerToString = {}
     stringToInteger = {}
-    for i in wordArray:
+    for i in wordList:
         if i not in integerToString.values():
             integerToString[counterITS] = i
             counterITS += 1
@@ -166,77 +166,77 @@ def createDictionary(wordArray):
 
     return [integerToString, stringToInteger]
 
-def count(wordArray, stringToInteger):
+def count(wordList, stringToInteger):
     '''
-    This creates 2D array counting the number of times a word follows another word
+    This creates a 2D list counting the number of times a word follows another word
     Parameters:
-        wordArray (string[]):  List of words
+        wordList (string[]):  List of words
         stringToInteger (dict):  Dictionary arranged by strings and storing integers
     Returns:
-        countArray (int[][]):  A 2D array counting the number of time a word follows another word
+        countList (int[][]):  A 2D list counting the number of time a word follows another word
     '''
     dictSize = len(stringToInteger)
-    countArray = [[0] * dictSize for _ in range(dictSize)] #https://stackoverflow.com/questions/13157961
+    countList = [[0] * dictSize for _ in range(dictSize)] #https://stackoverflow.com/questions/13157961
 
-    for i in range (0, len(wordArray) - 1):
-        firstWord = wordArray[i]
-        secondWord = wordArray[i+1]
+    for i in range (0, len(wordList) - 1):
+        firstWord = wordList[i]
+        secondWord = wordList[i+1]
         firstWordInt = stringToInteger[firstWord]
         secondWordInt = stringToInteger[secondWord]
-        countArray[firstWordInt][secondWordInt] += 1
+        countList[firstWordInt][secondWordInt] += 1
 
-    return countArray
+    return countList
 
-def rowTotals(countArray):
+def rowTotals(countList):
     '''
-    This creates an array of the sum of each row of given 2D array
+    This creates a list of the sum of each row of given 2D list
     Parameters:
-        countArray (int[][]):  2D array of numbers
+        countList (int[][]):  2D list of numbers
     Returns:
-        rowCountArray (int[]):  An array of the sum of each row of the given 2D array
+        rowCountList (int[]):  A list of the sum of each row of the given 2D list
     '''
-    rowCountArray = [0]*len(countArray)
+    rowCountList = [0]*len(countList)
     i = 0
-    for y in countArray:
+    for y in countList:
         for x in y:
-            rowCountArray[i] += x
+            rowCountList[i] += x
         i += 1
-    return rowCountArray
+    return rowCountList
 
-def calcProbabilities(countArray, rowCountArray):
+def calcProbabilities(countList, rowCountList):
     '''
-    This creates an array of the probability of a word following another word
+    This creates a list of the probability of a word following another word
     Parameters:
-        countArray (int[][]):  2D array of numbers
-        rowCountArray (int[]):  An array of the sum of each row of the given 2D array
+        countList (int[][]):  2D list of numbers
+        rowCountList (int[]):  A list of the sum of each row of the given 2D list
     Returns:
-        probArray (int[][]):  A 2D array of the probabilities that a given word with follow another word
+        probList (int[][]):  A 2D list of the probabilities that a given word with follow another word
     '''
-    arraySize = len(countArray)
-    probArray = [[0] * (arraySize) for _ in range(arraySize)] #https://stackoverflow.com/questions/13157961
+    listSize = len(countList)
+    probList = [[0] * (listSize) for _ in range(listSize)] #https://stackoverflow.com/questions/13157961
     xCount = 0
     yCount = 0
 
-    for y in countArray:
+    for y in countList:
         cumulativeProb = 0
         for x in y:
-            if (rowCountArray[yCount] != 0):
-                #print(round((x/rowCountArray[yCount]) + cumulativeProb, 2))
-                probArray[yCount][xCount] = round((x/rowCountArray[yCount]) + cumulativeProb, 2)
+            if (rowCountList[yCount] != 0):
+                #print(round((x/rowCountList[yCount]) + cumulativeProb, 2))
+                probList[yCount][xCount] = round((x/rowCountList[yCount]) + cumulativeProb, 2)
             else:
-                probArray[yCount][xCount] = cumulativeProb
-            cumulativeProb = probArray[yCount][xCount]
+                probList[yCount][xCount] = cumulativeProb
+            cumulativeProb = probList[yCount][xCount]
             xCount += 1
         xCount = 0
         yCount += 1
-    return probArray
+    return probList
 
-def generateTweet(integerToString, stringToInteger, firstWordArray, probArray, wordCount, punctCount):
+def generateTweet(integerToString, stringToInteger, firstWordList, probList, wordCount, punctCount):
     '''
-    This creates an array of the probability of a word following another word
+    This creates a list of the probability of a word following another word
     Parameters:
         integerToString (dict):  Dictionary arranged by integers and storing strings
-        probArray (int[][]):  A 2D array of the probabilities that a given word with follow another word
+        probList (int[][]):  A 2D list of the probabilities that a given word with follow another word
         wordCount (int):  The number of words to put in the tweet (this is a limit, it could stop earlier if there is punctuation)
     Returns:
         tweet (string):  A tweet which should mimic a Twitter user
@@ -246,7 +246,7 @@ def generateTweet(integerToString, stringToInteger, firstWordArray, probArray, w
     charCount = 0
     tweet = []
     #wordInt = randint(0, len(integerToString) - 1)
-    wordInt = stringToInteger[choice(firstWordArray)]
+    wordInt = stringToInteger[choice(firstWordList)]
     randomProb = 0
     tweetPunctCount = 0
     capitalize = True
@@ -283,8 +283,8 @@ def generateTweet(integerToString, stringToInteger, firstWordArray, probArray, w
         randomProb = random()
 
         for j in range(0, len(integerToString)):
-            #print(str(probArray[wordInt][j]) + " > " + str(randomProb) + " : " + integerToString[j])
-            if (probArray[wordInt][j] > randomProb):
+            #print(str(probList[wordInt][j]) + " > " + str(randomProb) + " : " + integerToString[j])
+            if (probList[wordInt][j] > randomProb):
                 wordInt = j
                 break
 
@@ -315,13 +315,13 @@ def printDictionary(dictionary):
     for item in dictionary:
         print("Key:{}, Value:{}".format(item, dictionary[item]))
 
-def print2dArray(arrayToPrint):
+def print2dList(listToPrint):
     '''
-    Print a 2D array
+    Print a 2D list
     Parameters:
-        arrayToPrint ([]):  Array to print
+        listToPrint ([]):  List to print
     '''
-    for x in arrayToPrint:
+    for x in listToPrint:
         print(x)
 
 def calculateMimic(twitterUser):
@@ -336,24 +336,24 @@ def calculateMimic(twitterUser):
 
     '''Get Tweets'''
     # print("Original text:")
-    #tweetArray = getTweetsTest("testData.txt")
-    tweetArray = readTweetsByUser(twitterUser, 200, False)
-    #print(tweetArray)
+    #tweetList = getTweetsTest("testData.txt")
+    tweetList = readTweetsByUser(twitterUser, 200, False)
+    #print(tweetList)
 
-    stats = getInputTweetsStats(tweetArray)
+    stats = getInputTweetsStats(tweetList)
     #print(stats)
     averageWords = stats["avgWords"]
     averagePunct = stats["avgPunct"]
     averageImages = stats["avgImg"] #Currently unused
 
-    splitWordsOutput = splitIntoWords(tweetArray)
-    wordArray = splitWordsOutput[0]
-    firstWordArray = splitWordsOutput[1]
-    #wordArray = tweetArray #For testing if you're getting data from a file
-    #print(wordArray)
+    splitWordsOutput = splitIntoWords(tweetList)
+    wordList = splitWordsOutput[0]
+    firstWordList = splitWordsOutput[1]
+    #wordList = tweetList #For testing if you're getting data from a file
+    #print(wordList)
 
     '''Create dictionaries for the tweets'''
-    dicts = createDictionary(wordArray)
+    dicts = createDictionary(wordList)
     # print("\nInteger to string:")
     integerToStringDict = dicts[0]
     # print(printDictionary(integerToStringDict))
@@ -363,35 +363,35 @@ def calculateMimic(twitterUser):
     # print("")
 
     '''Count the number of times a word follows another word'''
-    countArray = count(wordArray, stringToIntegerDict)
-    # print2dArray(countArray)
+    countList = count(wordList, stringToIntegerDict)
+    # print2dList(countList)
     # print("")
 
-    '''Sum of rows of the count array'''
-    rowCountArray = rowTotals(countArray)
-    # print(rowCountArray)
+    '''Sum of rows of the count list'''
+    rowCountList = rowTotals(countList)
+    # print(rowCountList)
     # print("")
 
     '''Calculate the probability of a word following another word'''
-    probArray = calcProbabilities(countArray, rowCountArray)
-    # print2dArray(probArray)
+    probList = calcProbabilities(countList, rowCountList)
+    # print2dList(probList)
     # print("")
 
     #Once this has all been generated pass it onto outputMimic
-    outputMimic(integerToStringDict, stringToIntegerDict, firstWordArray, probArray, averageWords, averagePunct, twitterUser)
+    outputMimic(integerToStringDict, stringToIntegerDict, firstWordList, probList, averageWords, averagePunct, twitterUser)
 
-def outputMimic(integerToStringDict, stringToIntegerDict, firstWordArray, probArray, averageWords, averagePunct, twitterUser):
+def outputMimic(integerToStringDict, stringToIntegerDict, firstWordList, probList, averageWords, averagePunct, twitterUser):
     '''
     Generate a tweet and give the option to output, try again or quit
     Parameters:
         integerToString (dict):  Dictionary arranged by integers and storing strings
         stringToInteger (dict):  Dictionary arranged by strings and storing integers
-        probArray (int[][]):  A 2D array of the probabilities that a given word with follow another word
+        probList (int[][]):  A 2D list of the probabilities that a given word with follow another word
         averageWords (int):  The average number of words in tweet
         averagePunct (float):  The average punctuation in a tweet
         twitterUser (string):  Twitter user you want to imitate
     '''
-    tweet = generateTweet(integerToStringDict, stringToIntegerDict, firstWordArray, probArray, averageWords, averagePunct)
+    tweet = generateTweet(integerToStringDict, stringToIntegerDict, firstWordList, probList, averageWords, averagePunct)
     # print(tweet)
     # print("")    
 
@@ -404,7 +404,7 @@ def outputMimic(integerToStringDict, stringToIntegerDict, firstWordArray, probAr
         print("The tweet was posted")
     elif outputCheck == "2":
         #Go again
-        outputMimic(integerToStringDict, stringToIntegerDict, firstWordArray, probArray, averageWords, averagePunct, twitterUser)
+        outputMimic(integerToStringDict, stringToIntegerDict, firstWordList, probList, averageWords, averagePunct, twitterUser)
     else:
         print("The tweet was not posted")
 
