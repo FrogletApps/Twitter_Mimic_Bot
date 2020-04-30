cdf6e0722f7db43b68fd283c31b56410d4cbccd6
James Pearson <james_pearson@outlook.com>
2020-04-30 14:59:17 +0100

    Can now reply to tweets (if they cannot be mimicked)

--- reply.py
+++ reply.py
@@ -17,19 +17,22 @@ with open(join(path[0], 'last_id.json'), 'r') as storeFile:
 search = twitter.search(q="@twimimicbot", since_id=last_id, count=1, tweet_mode='extended')
 
 username = ""
-tweet_id = 0
+requestUser = ""
+tweetIid = 0
 for tweetData in search["statuses"]:
+    print(tweetData)
     tweet = tweetData["full_text"]
-    tweet_id = tweetData["id"]
+    tweetId = tweetData["id"]
+    requestUser = "@" + tweetData["user"]["screen_name"]
     for word in tweet.split():
         if word[0] == "@" and "@twimimicbot" not in word:
             #We could go through the whole tweet and do multiple, but I don't want to spam anyone
             username = word
             break
 
-if username != "" and tweet_id != 0:
+if username != "" and tweetId != 0:
     print(username)
-    print(tweet_id)
+    print(tweetId)
     tweetNo = 0
     try:
         tweetNo = len(twitter.get_user_timeline(screen_name=username, count=200, include_rts=False))
@@ -39,13 +42,13 @@ if username != "" and tweet_id != 0:
         #Found some tweets, this means we can mimic it
         print(":)")
     else:
-        #No tweets or not enough tweets (not including retweets), cannot mimic this account
+        #Could not find enough tweets (not including retweets), cannot mimic this account
         print(":(")
+        twitter.update_status(status=requestUser + " Could not find enough tweets (not including retweets) to mimic " + username[1:], in_reply_to_status_id=tweetId)
 
     #Write the ID to file so we know where to search from next time
     try:
         last_id = search["statuses"][0]["id"]
-        last_id = 0 
         with open(join(path[0], 'last_id.json'), 'w') as storeFile:
             dump(last_id, storeFile)
     except:
