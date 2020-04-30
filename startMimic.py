42f6b8a35d93fadcefa41beac59db21281adaf72
James Pearson <james_pearson@outlook.com>
2020-04-30 16:06:36 +0100

    startMimic is now used to automatically generate tweets

---
+++ startMimic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from mimic import *
+
+#Insert a Twitter username here (leave this blank to randomly pick someone to mimic)
+userToMimic = ""
+
+calculateMimic(userToMimic)
