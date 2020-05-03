f251311b5a1b56476033d4a759c48d1673d57ded
James Pearson <james_pearson@outlook.com>
2020-05-03 20:15:22 +0100

    Commented out demo timing code

--- startMimic.py
+++ startMimic.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from mimic import *
+#from time import time
 
 #Insert a Twitter username here (leave this blank to randomly pick someone to mimic)
 userToMimic = ""
 
+#start = time()
 calculateMimic(userToMimic)
+#end = time()
+#print(end-start)
